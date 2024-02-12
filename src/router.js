91ab82ad074222bca60ffacf25c56b10fc3311f4
vinicius <vinicius.henrique.92@hotmail.com>
2024-02-12 19:05:14 -0300

    feat: create update service route

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ const ServiceController = require('./app/controllers/ServiceController');
 const router = Router();
 
 router.get('/services', ServiceController.index);
+router.get('/services/:id', ServiceController.show);
 router.post('/services', ServiceController.store);
+router.put('/services/:id', ServiceController.update);
 
 module.exports = router;
