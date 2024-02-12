6eb681755364c16c8aece17c95636b9e6d6655a5
vinicius <vinicius.henrique.92@hotmail.com>
2024-02-12 19:06:19 -0300

    feat: create update and get one service routes

--- src/app/controllers/ServiceController.js
+++ src/app/controllers/ServiceController.js
@@ -1,10 +1,27 @@
 const ServiceRepository = require('../repositories/ServiceRepository');
+const isValidUUID = require('../utils/isValidUUID');
 
 class ServiceController {
   async index(req, res) {
     const services = await ServiceRepository.findAll();
 
     res.json(services);
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    if (!isValidUUID(id)) {
+      return res.status(400).json({ error: 'Invalid service id' });
+    }
+
+    const service = await ServiceRepository.findById(id);
+
+    if (!service) {
+      return res.status(404).json({ error: 'Service not found' });
+    }
+
+    res.json(service);
   }
 
   async store(req, res) {
@@ -26,6 +43,36 @@ class ServiceController {
     res.status(201).json(service);
 
     res.send('post /services');
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { service_type, duration } = req.body;
+
+    if (!isValidUUID(id)) {
+      return res.status(400).json({ error: 'Invalid service id' });
+    }
+
+    if (!service_type) {
+      return res.status(400).json({ error: 'service type is required' });
+    }
+
+    if (!duration) {
+      return res.status(400).json({ error: 'duration is required' });
+    }
+    if (duration !== 30 && duration !== 60) {
+      return res.status(400).json({ error: 'duration must be 30 or 60' });
+    }
+
+    const serviceExists = await ServiceRepository.findById(id);
+
+    if (!serviceExists) {
+      return res.status(404).json({ error: 'Service not found' });
+    }
+
+    const service = await ServiceRepository.update(id, { service_type, duration });
+
+    res.json(service);
   }
 }
 
--- src/app/repositories/ServiceRepository.js
+++ src/app/repositories/ServiceRepository.js
@@ -7,12 +7,29 @@ class ServiceRepository {
     return rows;
   }
 
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM services where id = $1', [id]);
+
+    return row;
+  }
+
   async create({ service_type, duration }) {
     const [row] = await db.query(`
       INSERT INTO services(service_type, duration)
       VALUES($1, $2)
       RETURNING *
     `, [service_type, duration]);
+
+    return row;
+  }
+
+  async update(id, { service_type, duration }) {
+    const [row] = await db.query(`
+      UPDATE services
+      SET service_type = $1, duration = $2
+      WHERE id = $3
+      RETURNING *
+    `, [service_type, duration, id]);
 
     return row;
   }
