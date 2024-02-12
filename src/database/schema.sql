2fee2c4f25b7c10386191ca824b1d7ee9aac684d
vinicius <vinicius.henrique.92@hotmail.com>
2024-02-12 18:33:11 -0300

    feat: create database schema

---
+++ src/database/schema.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE galietastudios;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS services (
+	id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+	service_type VARCHAR NOT NULL UNIQUE,
+	duration INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS schedules (
+	id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+	name VARCHAR NOT NULL,
+	phone VARCHAR NOT NULL,
+	email VARCHAR NOT NULL,
+	schedule_date DATE NOT NULL,
+	hour TIME NOT NULL,
+	hour_end TIME NOT NULL,
+	available BOOLEAN DEFAULT TRUE,
+	service_id UUID NOT NULL,
+  FOREIGN KEY(service_id) REFERENCES services(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS users (
+	id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+	name VARCHAR NOT NULL,
+	phone VARCHAR NOT NULL,
+	email VARCHAR NOT NULL UNIQUE,
+	password VARCHAR NOT NULL
+);
